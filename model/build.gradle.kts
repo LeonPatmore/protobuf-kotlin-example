c34110e0f2f1b7c8b3e4d285ce4e4f80a29fb1b6
Leon Patmore <leon.patmore@bath.edu>
2023-01-14 17:24:11 +0000

    Update build.gradle.kts

--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -1,5 +1,4 @@
 import com.google.protobuf.gradle.*
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	kotlin("jvm") version "1.7.22"
@@ -12,14 +11,6 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 
-sourceSets {
-	main {
-		proto {
-			srcDir("src/main/proto")
-		}
-	}
-}
-
 repositories {
 	mavenCentral()
 }
@@ -29,27 +20,12 @@ dependencies {
 	protobuf(files("proto/"))
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
-}
-
 protobuf {
 	protoc {
 		artifact = "com.google.protobuf:protoc:3.21.12"
 	}
-	plugins {
-		id("grpckt") {
-			artifact = "io.grpc:protoc-gen-grpc-kotlin:1.3.0:jdk8@jar"
-		}
-	}
 	generateProtoTasks {
 		ofSourceSet("main").forEach {
-			it.plugins {
-				id("grpckt")
-			}
 			it.builtins {
 				id("kotlin")
 			}
