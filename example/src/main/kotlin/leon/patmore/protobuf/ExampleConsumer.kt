8f9fae82bd92bacc247358b7fc29d9ca5b8a4269
Leon Patmore <leon.patmore@bath.edu>
2023-01-16 17:51:45 +0000

    testing extended.

--- example/src/main/kotlin/leon/patmore/protobuf/ExampleConsumer.kt
+++ example/src/main/kotlin/leon/patmore/protobuf/ExampleConsumer.kt
@@ -2,15 +2,16 @@ package leon.patmore.protobuf
 
 import com.google.protobuf.kotlin.unpack
 import leon.patmore.protobuf.model.QueueModel.ExampleDomain
-import leon.patmore.protobuf.model.QueueModel.QueueMessage
+import leon.patmore.protobuf.model.QueueModel.QueueMessageExtended
 import org.springframework.stereotype.Component
-import java.util.Base64
+import java.util.*
 
 @Component
 class ExampleConsumer : Consumer {
 
     override fun process(message: String) {
-        val queueMessage = QueueMessage.parseFrom(Base64.getDecoder().decode(message))
+        val queueMessage = QueueMessageExtended.parseFrom(Base64.getDecoder().decode(message))
+        println(queueMessage.newField)
         val exampleDomain = queueMessage.content.unpack<ExampleDomain>()
         println(exampleDomain.status)
     }
