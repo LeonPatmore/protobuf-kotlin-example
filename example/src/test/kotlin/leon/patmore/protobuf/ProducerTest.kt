24d5239e2466d9c6dd882038db6cc274eb9c8fc7
Leon Patmore <leon.patmore@bath.edu>
2023-01-18 23:03:09 +0000

    Update ProducerTest.kt

--- example/src/test/kotlin/leon/patmore/protobuf/ProducerTest.kt
+++ example/src/test/kotlin/leon/patmore/protobuf/ProducerTest.kt
@@ -1,6 +1,8 @@
 package leon.patmore.protobuf
 
 import com.google.protobuf.Any
+import io.kotest.matchers.shouldBe
+import leon.patmore.protobuf.model.QueueModel
 import leon.patmore.protobuf.model.QueueModel.ExampleDomain
 import leon.patmore.protobuf.model.QueueModel.QueueMessage
 import leon.patmore.protobuf.simple.SimpleConfiguration
@@ -21,6 +23,24 @@ internal class ProducerTest {
                 .setTtlMs(10000)
                 .setContent(Any.pack(ExampleDomain.newBuilder().setStatus("hello").build()))
                 .build())
+    }
+
+    @Test
+    internal fun test() {
+        val fullObject = QueueModel.QueueMessageExtended.newBuilder()
+                .setId("id")
+                .setNewField("some-value")
+                .build()
+
+        val fullArray = fullObject.toByteArray()
+
+        val subObject = QueueMessage.parseFrom(fullArray)
+        subObject.id shouldBe "id"
+
+        val subObjectBytes = subObject.toByteArray()
+        val fullObject2 = QueueModel.QueueMessageExtended.parseFrom(subObjectBytes);
+        fullObject2.id shouldBe "id"
+        fullObject2.newField shouldBe "some-value"
     }
 
 }
